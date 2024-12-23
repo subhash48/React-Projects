1f2a19a56e4032cf652d38a8473bc76ec8c458fe
Subhash Galla <subhashgalla@Subhashs-MacBook-Pro-2.local>
2024-12-22 19:31:46 -0600

    Added New styles for todolist project and updated app.jsx flie, finally completed the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+// import Counter from "./components/Counter";
+import Todo from "./components/Todo";
 
 export const App = () => {
   return (
-    <div>App</div>
+    <div>
+       {/* <Counter/> */}
+       <Todo/>
+    </div>
   )
 }
+
+export default App;
