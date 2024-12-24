51dbe36bef4dcff97ce45f1901248553f286ef36
Subhash Galla <subhashgalla@Subhashs-MacBook-Pro-2.local>
2024-12-24 13:21:31 -0600

    Updated styel sheet and added Project 9 Form

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@
 //import ToggleBackgroundColor from "./components/ToggleBackgroundColor";
 //import HiddenSearchBar from "./components/HiddenSearchBar";
 //import Testimonials from "./components/Testimonials";
-import Accordion from "./components/Accordion";
-import { accordionData } from "./utils/content";
+// import Accordion from "./components/Accordion";
+// import { accordionData } from "./utils/content";
+
+import Form from "./components/Form";
 
 const App = () => {
   return (
@@ -19,11 +21,13 @@ const App = () => {
        {/* <ToggleBackgroundColor/> */}
        {/* <HiddenSearchBar/> */}
        {/* <Testimonials/> */}
-      <div className="accordion">
+       {/* <div className="accordion">
           {accordionData.map(({title, content}) => (
               <Accordion title={title} content={content}/>
-        ))}
-      </div>
+          ))}
+        </div> */}
+
+      <Form />
     </div>
 
   )
